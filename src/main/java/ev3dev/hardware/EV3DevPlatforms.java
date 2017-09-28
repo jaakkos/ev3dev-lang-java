a379a53500cc28422fe1fe3a84ab63323e94c814
Jaakko Suutarla <jaakko@suutarla.com>
2017-09-28 19:51:39 +0300

    bypass platform detection and use brickpi3

--- src/main/java/ev3dev/hardware/EV3DevPlatforms.java
+++ src/main/java/ev3dev/hardware/EV3DevPlatforms.java
@@ -30,86 +30,25 @@ public abstract class EV3DevPlatforms extends EV3DevFileSystem {
         final String BRICKPI_DISCOVERY_PATTERN_PATH = BATTERY_PATH + "/" + BATTERY_BRICKPI;
         final String BRICKPI3_DISCOVERY_PATTERN_PATH = BATTERY_PATH + "/" + BATTERY_BRICKPI3;
 
-        if(Sysfs.existPath(EV3BRICK_DISCOVERY_PATTERN_PATH)){
-            if(log.isTraceEnabled())
-                log.trace(EV3BRICK_DISCOVERY_PATTERN_PATH);
-                log.trace("Detected platform: " + EV3DevPlatform.EV3BRICK);
-            return EV3DevPlatform.EV3BRICK;
-        } else if(Sysfs.existPath(PISTORMS_DISCOVERY_PATTERN_PATH)){
-            if(log.isTraceEnabled())
-                log.trace(PISTORMS_DISCOVERY_PATTERN_PATH);
-                log.trace("Detected platform: " + EV3DevPlatform.PISTORMS);
-            return EV3DevPlatform.PISTORMS;
-        } else if(Sysfs.existPath(BRICKPI_DISCOVERY_PATTERN_PATH)){
-            if(log.isTraceEnabled())
-                log.trace(BRICKPI_DISCOVERY_PATTERN_PATH);
-                log.trace("Detected platform: " + EV3DevPlatform.BRICKPI);
-            return EV3DevPlatform.BRICKPI;
-        } else if(Sysfs.existPath(BRICKPI3_DISCOVERY_PATTERN_PATH)){
-            if(log.isTraceEnabled())
-                log.trace(BRICKPI3_DISCOVERY_PATTERN_PATH);
-                log.trace("Detected platform: " + EV3DevPlatform.BRICKPI3);
-            return EV3DevPlatform.BRICKPI3;
-        } else {
-            final String OS_NAME = System.getProperty("os.name");
-            final String OS_VERSION = System.getProperty("os.version");
-            final String message = "Platform not supported: " + OS_NAME + " " + OS_VERSION;
-            log.error(message);
-            throw new RuntimeException(message);
-        }
+
+        if(log.isTraceEnabled())
+            log.trace(BRICKPI3_DISCOVERY_PATTERN_PATH);
+            log.trace("Detected platform: " + EV3DevPlatform.BRICKPI3);
+        return EV3DevPlatform.BRICKPI3;
     }
 
     protected String getMotorPort(final Port port) {
 
-        if(this.getPlatform().equals(EV3DevPlatform.EV3BRICK)){
-
-            if(port.equals(MotorPort.A)){
-                return "outA";
-            }else if(port.equals(MotorPort.B)){
-                return "outB";
-            }else if(port.equals(MotorPort.C)){
-                return "outC";
-            }else if(port.equals(MotorPort.D)){
-                return "outD";
-            }
-
-        } else if(this.getPlatform().equals(EV3DevPlatform.BRICKPI)) {
-
-            if (port.equals(MotorPort.A)) {
-                return "ttyAMA0:MA";
-            } else if (port.equals(MotorPort.B)) {
-                return "ttyAMA0:MB";
-            } else if (port.equals(MotorPort.C)) {
-                return "ttyAMA0:MC";
-            } else if (port.equals(MotorPort.D)) {
-                return "ttyAMA0:MD";
-            }
-
-        } else if(this.getPlatform().equals(EV3DevPlatform.BRICKPI3)) {
-
-            if (port.equals(MotorPort.A)) {
-                return "spi0.1:MA";
-            } else if (port.equals(MotorPort.B)) {
-                return "spi0.1:MB";
-            } else if (port.equals(MotorPort.C)) {
-                return "spi0.1:MC";
-            } else if (port.equals(MotorPort.D)) {
-                return "spi0.1:MD";
-            }
-
-        } else {
-
-            if (port.equals(MotorPort.A)) {
-                return "pistorms:BBM1";
-            } else if (port.equals(MotorPort.B)) {
-                return "pistorms:BBM2";
-            } else if (port.equals(MotorPort.C)) {
-                return "pistorms:BAM2";
-            } else if (port.equals(MotorPort.D)) {
-                return "pistorms:BAM1";
-            }
-
+        if (port.equals(MotorPort.A)) {
+            return "spi0.1:MA";
+        } else if (port.equals(MotorPort.B)) {
+            return "spi0.1:MB";
+        } else if (port.equals(MotorPort.C)) {
+            return "spi0.1:MC";
+        } else if (port.equals(MotorPort.D)) {
+            return "spi0.1:MD";
         }
+
 
         //TODO Improve
         return null;
@@ -117,55 +56,16 @@ public abstract class EV3DevPlatforms extends EV3DevFileSystem {
 
     protected String getSensorPort(final Port port) {
 
-        if(this.getPlatform().equals(EV3DevPlatform.EV3BRICK)){
-
-            if(port.equals(SensorPort.S1)){
-                return "in1";
-            }else if(port.equals(SensorPort.S2)){
-                return "in2";
-            }else if(port.equals(SensorPort.S3)){
-                return "in3";
-            }else if(port.equals(SensorPort.S4)){
-                return "in4";
-            }
-
-        } else if(this.getPlatform().equals(EV3DevPlatform.BRICKPI)) {
-
-            if (port.equals(SensorPort.S1)) {
-                return "ttyAMA0:S1";
-            } else if (port.equals(SensorPort.S2)) {
-                return "ttyAMA0:S2";
-            } else if (port.equals(SensorPort.S3)) {
-                return "ttyAMA0:S3";
-            } else if (port.equals(SensorPort.S4)) {
-                return "ttyAMA0:S4";
-            }
-
-        } else if(this.getPlatform().equals(EV3DevPlatform.BRICKPI3)) {
-
-            if (port.equals(SensorPort.S1)) {
-                return "spi0.1:S1";
-            } else if (port.equals(SensorPort.S2)) {
-                return "spi0.1:S2";
-            } else if (port.equals(SensorPort.S3)) {
-                return "spi0.1:S3";
-            } else if (port.equals(SensorPort.S4)) {
-                return "spi0.1:S4";
-            }
-
-        } else {
-
-            if (port.equals(SensorPort.S1)) {
-                return "pistorms:BBS2";
-            } else if (port.equals(SensorPort.S2)) {
-                return "pistorms:BBS1";
-            } else if (port.equals(SensorPort.S3)) {
-                return "pistorms:BAS1";
-            } else if (port.equals(SensorPort.S4)) {
-                return "pistorms:BAS2";
-            }
-
+        if (port.equals(SensorPort.S1)) {
+            return "spi0.1:S1";
+        } else if (port.equals(SensorPort.S2)) {
+            return "spi0.1:S2";
+        } else if (port.equals(SensorPort.S3)) {
+            return "spi0.1:S3";
+        } else if (port.equals(SensorPort.S4)) {
+            return "spi0.1:S4";
         }
+
 
         //TODO Improve
         return null;
